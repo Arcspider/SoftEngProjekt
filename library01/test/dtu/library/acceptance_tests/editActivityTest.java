c10595f0c9a60153b3d2d23b4fe3dab7989bd80d
Toibas <toibas.lundsgaard@gmail.com>
2020-04-26 13:38:21 +0200

    ad

---
+++ library01/test/dtu/library/acceptance_tests/editActivityTest.java
@@ -0,0 +1,51 @@
+package dtu.library.acceptance_tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import dtu.library.app.Activity;
+import dtu.library.app.Controller;
+import dtu.library.app.Model;
+import dtu.library.app.OperationNotAllowedException;
+import dtu.library.app.Project;
+import dtu.library.app.View;
+import dtu.library.app.controllerInterface.ControllerActivity;
+import dtu.library.app.controllerInterface.ControllerProject;
+import io.cucumber.java.en.*;
+
+public class editActivityTest {
+
+	private View view;
+	private Model model;
+	private Controller controller;
+	private ControllerProject controllerProject;
+	private ControllerActivity controllerActivity;
+	private ErrorMessageHolder errorMessageHolder;
+	private String name;
+
+	private Project project;
+	Activity newActivity;
+
+	public editActivityTest(View view, Model model, ErrorMessageHolder errorMessageHolder, Controller controller,
+			ControllerProject controllerProject, ControllerActivity controllerActivity) {
+		this.view = view;
+		this.model = model;
+		this.errorMessageHolder = errorMessageHolder;
+		this.controller = controller;
+		this.controllerProject = controllerProject;
+		this.controllerActivity = controllerActivity;
+	}
+//	@Given("a project with id {string} has and activity {string}")
+//	public void aProjectWithIdHasAndActivity(String id, String name) throws OperationNotAllowedException {
+//	    project = controller.createProject(name);
+//	    project.setId(id);
+//	    controller.addProject(project);
+//	    controllerActivity.addActivity(name, project);
+//	}
+//	
+//	@Given("the project has start date {string} and end data {string}")
+//	public void theProjectHasStartDateAndEndData(String startDate, String endDate) {
+//		controllerProject.setProjectTime(project, startDate, endDate);
+//	}
+}
