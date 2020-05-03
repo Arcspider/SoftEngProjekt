c23c153440b491b7b43909152681a026e65f869a
s194579 <s194579@student.dtu.dk>
2020-05-03 15:31:34 +0200

    FixedDates

--- library01/src/dtu/library/app/ModelActivity.java
+++ library01/src/dtu/library/app/ModelActivity.java
@@ -28,7 +28,7 @@ public class ModelActivity {
 			if (endActivityDate == null || startActivityDate.isBefore(endActivityDate)) {
 				LocalDate newStart = stringToDate(startDate);
 				project.setActivityStartDate(currentActivity, newStart);
-				System.out.println(thisActivity.toString());
+				System.out.println(currentActivity.toString());
 			} else
 				throw new OperationNotAllowedException("End date is before start date");
 		}
@@ -55,7 +55,7 @@ public class ModelActivity {
 			if (endActivityDate == null || startActivityDate.isBefore(endActivityDate)) {
 				LocalDate newEnd = stringToDate(endDate);
 				project.setActivityEndDate(currentActivity, newEnd);
-				System.out.println(thisActivity.toString());
+				System.out.println(currentActivity.toString());
 			} else
 				throw new OperationNotAllowedException("Start date is after end date");
 		}
